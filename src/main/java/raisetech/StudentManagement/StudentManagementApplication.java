5a09cb5b220d6fa4a0723821d2c7ace16a6cefc0
Naoki Takayama <37gm.pkmn@gmail.com>
2025-12-28 20:21:49 +0900

    Merge pull request #1 from NaokiTakayama-max/test
    text.txtを作成

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -3,6 +3,7 @@ package raisetech.StudentManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentManagementApplication {
 
   private String name = "Naoki Takayama";
+  private String age = "23";
 
   public static void main(String[] args) {
     SpringApplication.run(StudentManagementApplication.class, args);
@@ -20,5 +22,31 @@ public class StudentManagementApplication {
     return name;
   }
 
+  @GetMapping("/age")
+  public String getAge() {
+    return age;
+  }
+
+  @GetMapping("/studentInfo")
+  public String getStudentInfo() {
+    return name + " " + age + "歳";
+  }
+
+
+  @PostMapping("/name")
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @PostMapping("/age")
+  public void setAge(String age) {
+    this.age = age;
+  }
+
+  @PostMapping("/studentInfo")
+  public void setStudentInfo(String name, String age) {
+    this.name = name;
+    this.age = age;
+  }
 
 }
