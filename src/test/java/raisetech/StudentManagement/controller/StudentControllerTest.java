5aba90614a3b07742fe503f2dd00ef5c2fe7b4c5
Naoki Takayama <37gm.pkmn@gmail.com>
2026-05-27 18:50:41 +0900

    Merge remote-tracking branch 'origin/master1' into master1
    # Conflicts:
    #	src/main/resources/application.properties

--- src/test/java/raisetech/StudentManagement/controller/StudentControllerTest.java
+++ src/test/java/raisetech/StudentManagement/controller/StudentControllerTest.java
@@ -1,5 +1,7 @@
 package raisetech.StudentManagement.controller;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -9,8 +11,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +42,8 @@ class StudentControllerTest {
 
   @MockBean
   private StudentRepository repository;
+
+  private Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
   //HTTPリクエストを送る
   @Autowired
@@ -64,6 +74,55 @@ class StudentControllerTest {
     return detail;
   }
 
+  @Test
+  void 受講生詳細の受講生で適切な値を入力したときに入力チェックに異常が発生しないこと() {
+    Student student = new Student();
+    student.setStudentId("1");
+    student.setName("山田太郎");
+    student.setFurigana("タロウ");
+    student.setNickname("たろう");
+    student.setGender("男性");
+    student.setAge(20);
+    student.setMailAddress("taro@test.com");
+    student.setRegion("東京");
+
+    Set<ConstraintViolation<Student>> violations = validator.validate(student);
+
+    assertThat(violations.size()).isEqualTo(0);
+  }
+
+  @Test
+  void 受講生詳細の受講生でIDに数字以外を用いたときに入力チェックに掛かること() {
+    Student student = new Student();
+    student.setStudentId("テストです。");
+    student.setName("山田太郎");
+    student.setFurigana("タロウ");
+    student.setNickname("たろう");
+    student.setGender("男性");
+    student.setAge(20);
+    student.setMailAddress("taro@test.com");
+    student.setRegion("東京");
+
+    Set<ConstraintViolation<Student>> violations = validator.validate(student);
+
+    assertThat(violations.size()).isEqualTo(1);
+    //assertThat(violations).extracting("message").containsOnly("数字のみ入力するようにしてください。");
+  }
+
+  @Test
+  void 受講生のコース情報が空欄の場合に入力チェックに掛かること() {
+    StudentsCourses course = new StudentsCourses();
+    course.setCourseName("");
+    course.setStudentId("1");
+    course.setStudentsCoursesId("SC001");
+    course.setStartDate("2026-02-01");
+    course.setPlannedEndDate("2026-08-31");
+
+    Set<ConstraintViolation<StudentsCourses>> violations = validator.validate(course);
+
+    assertThat(violations.size()).isEqualTo(1);
+  }
+
   //受講生一覧検索のテスト
   @Test
   void 受講生情報の一覧検索が成功すること() throws Exception {
@@ -72,12 +131,28 @@ class StudentControllerTest {
 
     when(service.searchStudentList()).thenReturn(studentDetailList);
 
+    //JSONの配列を指定する場合は"$[0]…"
     mockMvc.perform(get("/studentList"))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$[0].student.studentId").value("1"))
         .andExpect(jsonPath("$[0].student.name").value("Taro"));
 
     verify(service).searchStudentList();
+  }
+
+  @Test
+  void 受講生詳細検索が成功すること() throws Exception {
+    StudentDetail detail = createStudentDetail("1", "Taro");
+
+    when(service.searchStudentDetail("1")).thenReturn(detail);
+
+    //JSONの単体オブジェクトを指定する場合は"$…"
+    mockMvc.perform(get("/student/1"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.student.studentId").value("1"))
+        .andExpect(jsonPath("$.student.name").value("Taro"));
+
+    verify(service).searchStudentDetail("1");
   }
 
   //受講生情報の登録のテスト
